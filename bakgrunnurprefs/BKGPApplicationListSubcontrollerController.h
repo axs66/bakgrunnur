0f62fe830db40fc0c4a607655a3aa37089a6d59c
A先生 <142523398+axs66@users.noreply.github.com>
2025-10-14 21:10:51 +0800

    Update BKGPApplicationListSubcontrollerController.h

--- bakgrunnurprefs/BKGPApplicationListSubcontrollerController.h
+++ bakgrunnurprefs/BKGPApplicationListSubcontrollerController.h
@@ -1,13 +1,6 @@
 #import "../common.h"
 
-#if __has_include(<AltList/ATLApplicationListSubcontrollerController.h>)
-#import <AltList/ATLApplicationListSubcontrollerController.h>
-#else
-#import <Preferences/PSListController.h>
-@interface ATLApplicationListSubcontrollerController : PSListController
--(PSSpecifier *)specifierForApplicationWithIdentifier:(NSString *)identifier;
-@end
-#endif
+#import "AltList/ATLApplicationListSubcontrollerController.h"
 
 @interface BKGPApplicationListSubcontrollerController : ATLApplicationListSubcontrollerController{
     NSMutableDictionary *_prefs;
