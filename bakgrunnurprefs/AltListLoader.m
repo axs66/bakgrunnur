c52558d15e783be23baeab62773b6caa75d10195
A先生 <142523398+axs66@users.noreply.github.com>
2025-10-15 10:59:24 +0800

    Update AltListLoader.m

--- bakgrunnurprefs/AltListLoader.m
+++ bakgrunnurprefs/AltListLoader.m
@@ -2,7 +2,7 @@
 
 __attribute__((constructor)) static void loadAltListAtStartup(void){
     const char *candidates[] = {
-        // Rootless framework paths
+        // Rootless framework paths (most common)
         "/var/jb/Library/Frameworks/AltList.framework/AltList",
         "/var/jb/System/Library/Frameworks/AltList.framework/AltList",
         // Rootful framework paths
@@ -11,7 +11,10 @@ __attribute__((constructor)) static void loadAltListAtStartup(void){
         // Rootless bundle paths
         "/var/jb/Library/PreferenceBundles/AltList.bundle/AltList",
         // Rootful bundle paths
-        "/Library/PreferenceBundles/AltList.bundle/AltList"
+        "/Library/PreferenceBundles/AltList.bundle/AltList",
+        // Dopamine specific paths
+        "/private/preboot/procursus/Library/Frameworks/AltList.framework/AltList",
+        "/private/preboot/procursus/Library/PreferenceBundles/AltList.bundle/AltList"
     };
     for (unsigned i = 0; i < sizeof(candidates)/sizeof(candidates[0]); i++){
         void *h = dlopen(candidates[i], RTLD_NOW | RTLD_GLOBAL);
