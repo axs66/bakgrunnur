2c1709ffc7b74578bdbcefe4a245bd4ee139d6b2
A先生 <142523398+axs66@users.noreply.github.com>
2025-10-24 19:58:50 +0800

    Update BKGPApplicationListSubcontrollerController.h

--- bakgrunnurprefs/BKGPApplicationListSubcontrollerController.h
+++ bakgrunnurprefs/BKGPApplicationListSubcontrollerController.h
@@ -1,9 +1,13 @@
 #import "../common.h"
 #import <Preferences/PSListController.h>
 
+// Forward declaration to avoid compile-time dependency on AltList
+@class ATLApplicationListSubcontrollerController;
+
 @interface BKGPApplicationListSubcontrollerController : PSListController {
     NSMutableDictionary *_prefs;
     NSArray *_allEntriesIdentifier;
+    id _altListController;
 }
 -(void)updateIvars;
 -(void)loadPreferences;
